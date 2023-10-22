08411484a7a9efb7f845f4217029b82787a7311e
Thompsen1009 <133374360+Thompsen1009@users.noreply.github.com>
2023-10-22 18:51:34 +0200

    test

--- src/pages/Home_page.jsx
+++ src/pages/Home_page.jsx
@@ -1,18 +1,9 @@
 import React from 'react';
-import Hero from '../components/Home_Hero';
-import Subjects from '../components/Home_Subjects'; 
-import Mission from '../components/Home_Mission';
-import Process from '../components/Home_Process';
-import Contact from '../components/Home_Contact';
 
 function Home() {
   return (
-    <div>
-      <Hero />
-      <Subjects />
-      <Mission />
-      <Process />
-      <Contact />
+    <div className='text-white text-2xl bg-black'>
+      <h1>Test</h1>
     </div>
   );
 }
